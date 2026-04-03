acd9b998774f9c1a05af6fa01737ddc4c5666e01
JustinSylt <justin.sylt@outlook.de>
2026-04-04 00:14:54 +0200

    Add: Dialog open and close

--- script.js
+++ script.js
@@ -13,8 +13,20 @@ const ARRAY_OF_IMAGES = [
 function renderAllImages(images) {
 	let imageContainerRef = document.getElementById('image-container');
 	for (i = 0; i < images.length; i++) {
-		imageContainerRef.innerHTML += `<img class="gallery-images" src="./assets/img/${images[i]}"></img>`;
+		imageContainerRef.innerHTML += `<img class="gallery-images" src="./assets/img/${images[i]}" onclick='openDialog()'></img>`;
 	}
 }
 
 renderAllImages(ARRAY_OF_IMAGES);
+
+//function that renders a single image inside of a dialog
+function openDialog() {
+	let dialogRef = document.getElementById('img-dialog');
+	dialogRef.showModal();
+}
+
+//close Dialog function
+function closeDialog() {
+	let dialogRef = document.getElementById('img-dialog');
+	dialogRef.close();
+}
