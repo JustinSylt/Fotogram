fdc803151763909aa11f22ff53b725d5c68f0d2f
JustinSylt <justin.sylt@outlook.de>
2026-04-03 23:31:50 +0200

    Style: Grid Layout

--- script.js
+++ script.js
@@ -4,20 +4,18 @@ const ARRAY_OF_IMAGES = [
 	'dog_in_snow.jpg',
 	'eagle.jpg',
 	'gecko.jpg',
-	'hot_air_baloon.jpg',
 	'iceland_polarlights.jpg',
 	'iceland_waterfalls.jpg',
 	'kitten.jpg',
 	'mountain_skyline.jpg',
 	'penguin.jpg',
-	'rainbow.jpg',
 ];
 
 //function that renders all images on website
 function renderAllImages(images) {
 	let imageContainerRef = document.getElementById('image-container');
 	for (i = 0; i < images.length; i++) {
-		imageContainerRef.innerHTML += `<img class="gallery-images" src="./assets/img/${images[i]}">${images[i]}</img>`;
+		imageContainerRef.innerHTML += `<img class="gallery-images" src="./assets/img/${images[i]}"></img>`;
 	}
 }
 
