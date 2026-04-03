425805ad84a15ccb25c3b15ef7a19abb43b3c3d2
JustinSylt <justin.sylt@outlook.de>
2026-04-03 21:05:18 +0200

    Add: render all images to screen

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-// returns an array of all image names for the website
-function getAllImages() {
-	return imgArray;
+const ARRAY_OF_IMAGES = [
+	'black_cat.jpg',
+	'deer.jpg',
+	'dog_in_snow.jpg',
+	'eagle.jpg',
+	'gecko.jpg',
+	'hot_air_baloon.jpg',
+	'iceland_polarlights.jpg',
+	'iceland_waterfalls.jpg',
+	'kitten.jpg',
+	'mountain_skyline.jpg',
+	'penguin.jpg',
+	'rainbow.jpg',
+];
+
+//function that renders all images on website
+function renderAllImages(images) {
+	let imageContainerRef = document.getElementById('image-container');
+	for (i = 0; i < images.length; i++) {
+		imageContainerRef.innerHTML += `<img class="gallery-images" src="./assets/img/${images[i]}">${images[i]}</img>`;
+	}
 }
+
+renderAllImages(ARRAY_OF_IMAGES);
