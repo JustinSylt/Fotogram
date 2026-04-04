98fd7539dbca89d0d0afd2047c056abfa8f9b487
JustinSylt <justin.sylt@outlook.de>
2026-04-04 20:50:35 +0200

    Style: Dialog styling finished

--- script.js
+++ script.js
@@ -30,11 +30,28 @@ function openDialog(event) {
 //function that renders a single image inside of a dialog
 function renderImageInDialog(src) {
 	let dialogImgContainerRef = document.getElementById('dialog-img-container');
-	dialogImgContainerRef.innerHTML = `<img class='single-dialog-img' src='./assets/img/${src}'></img>`;
+	let slideshowDialogRef = document.getElementById('img-dialog');
+	let dialogHeaderRef = document.getElementById('dialog-header');
+
+	dialogHeaderRef.innerHTML = `<h2>${src}</h2>`;
+	slideshowDialogRef.classList.add('open');
+	dialogImgContainerRef.innerHTML = `
+	<div class='single-dialog-img-container'>
+		<img class='arrow-icon' src='./assets/icons/left-arrow.png'></img>
+		<img class='single-dialog-img' src='./assets/img/${src}'></img>
+		<img class='arrow-icon' src='./assets/icons/right-arrow.png'></img>
+	</div>`;
 }
 
 //close Dialog function
 function closeDialog() {
 	let dialogRef = document.getElementById('img-dialog');
+	let slideshowDialogRef = document.getElementById('img-dialog');
+	slideshowDialogRef.classList.remove('open');
 	dialogRef.close();
 }
+
+function switchRightImage() {}
+function switchLeftImage() {}
+
+document.getElementById('img-dialog').addEventListener('close', closeDialog);
